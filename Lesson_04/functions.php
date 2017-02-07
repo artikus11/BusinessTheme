74eedaec2c871711fdb82758f11f35a27d2a9f07
Artem Abramovich <artikus.sol@gmail.com>
2017-02-07 17:26:37 +0400

    Lesson 04

---
+++ Lesson_04/functions.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Required: set 'ot_theme_mode' filter to true.
+ */
+add_filter( 'ot_theme_mode', '__return_true' );
+add_filter( 'ot_show_new_layout', '__return_false' );
+add_filter( 'ot_show_pages', '__return_true' );
+
+
+/**
+ * Required: include OptionTree.
+ */
+require( trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php' );
+require( trailingslashit( get_template_directory() ) . 'functions/theme-options.php' );
+require( trailingslashit( get_template_directory() ) . 'functions/meta-boxes.php' );
+
+function theme_options_parent($parent ) {
+	$parent = '';
+	return $parent;
+}
+add_filter( 'ot_theme_options_parent_slug', 'theme_options_parent',20 );
+
+
+function artbt_setup() {
+
+	add_theme_support( 'title-tag' );
+
+	add_theme_support( 'post-thumbnails' );
+
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Primary', 'artbt' ),
+	) );
+
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+
+}
+add_action( 'after_setup_theme', 'artbt_setup' );
+
+function artbt_widgets_init() {
+	register_sidebar( array(
+		'name'          => 'Область виджетов',
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'artbt' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	register_sidebar( array(
+		'name'          => 'Подвал слева',
+		'id'            => 'footer-left',
+		'description'   => '',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<div class="widget-title">',
+		'after_title'   => '</div>',
+	) );
+	register_sidebar( array(
+		'name'          => 'Подвал центр',
+		'id'            => 'footer-center',
+		'description'   => '',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<div class="widget-title">',
+		'after_title'   => '</div>',
+	) );
+	register_sidebar( array(
+		'name'          => 'Подвал справа',
+		'id'            => 'footer-right',
+		'description'   => '',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<div class="widget-title">',
+		'after_title'   => '</div>',
+	) );
+}
+add_action( 'widgets_init', 'artbt_widgets_init' );
+
+
+function artbt_style() {
+	wp_enqueue_style( 'grid', get_template_directory_uri() . '/css/grid.css');
+	wp_enqueue_style( 'artbt-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'font', 'http://fonts.googleapis.com/css?family=Roboto:400,500,700');
+	wp_enqueue_style( 'font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
+	wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css');
+	wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css');
+	wp_enqueue_style( 'colorScheme', get_template_directory_uri() . '/css/colorScheme-2.css');
+	wp_enqueue_style( 'carousel', get_template_directory_uri() . '/css/owl-carousel.css');
+}
+add_action( 'wp_enqueue_scripts', 'artbt_style' );
+
+
+
+function artbt_scripts() {
+	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/js/script.js', array ('jquery'), '', true );
+	wp_enqueue_script( 'rd-navbar', get_template_directory_uri() . '/js/jquery.rd-navbar.js', array ('jquery'), '', true );
+	wp_enqueue_script( 'superfish', get_template_directory_uri() . '/js/superfish.js', array ('jquery'), '', true );
+	wp_enqueue_script( 'tmstickup', get_template_directory_uri() . '/js/tmstickup.js', array ('jquery'), '', true );
+	wp_enqueue_script( 'magnific-popup-script', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array ('jquery'), '', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'artbt_scripts' );
+
+
+function artbt_filter_current_item_menu_header ($classes) {
+	if (in_array('current-menu-item', $classes)){
+		$classes[] = 'active';
+	}
+	return $classes;
+}
+add_filter('nav_menu_css_class', 'artbt_filter_current_item_menu_header'  );
---
+++ Lesson_04/page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Business
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+      <main>
+        <section class="well1">
+          <div class="container">
+            <h2>Privacy Policy</h2>
+            <h3 class="off1 primary">Aliquam malesuada urna eu dolor volutpat lobortis curabitur pellentesque</h3>
+            <p>Suspendisse commodo tempor sagittis! In justo est, sollicitudin eu scelerisque pretium, placerat eget elit. Praesent faucibus rutrum odio at rhoncus. Pellentesque vitae tortor id neque fermentum pretium. Maecenas ac lacus ut neque rhoncus laoreet sed id tellus. Donec justo tellus, tincidunt vitae pellentesque nec, pharetra a orci. Praesent nunc nunc, egestas eget elementum sed; rutrum eget metus! Vestibulum congue congue dui ut porta. Aenean laoreet viverra turpis, a commodo purus eleifend a. Nam lacus dui; egestas ut aliquam nec, eleifend sit amet est. Nam justo elit, dictum id tempus a, ultricies tempus lacus. Nunc purus nibh; eleifend eget facilisis ac, sagittis non tortor. Vivamus eu enim a orci accumsan tincidunt ut ut elit. Vestibulum nisi orci, rutrum ac auctor non, viverra et magna? Sed vehicula sem et arcu tincidunt accumsan. In non pellentesque ipsum. Curabitur facilisis pellentesque pharetra.<br/><br/>Donec justo urna, malesuada a viverra ac, pellentesque vitae nunc. Aenean ac leo eget nunc fringilla fringilla a non nulla! Nunc orci mi, venenatis quis ultrices vitae, congue non nibh. Nulla bibendum, justo eget ultrices vestibulum, erat tortor venenatis risus, sit amet cursus dui augue a arcu. Quisque mauris risus, gravida a molestie eu, dictum ac augue. Integer sodales tempor lectus; sit amet dictum metus pharetra nec. Fusce bibendum dapibus pretium. Nunc eu sem vitae lacus laoreet elementum. Nulla facilisi. Phasellus ullamcorper elementum est, id pretium turpis bibendum vel. Aliquam a odio arcu. Morbi a dignissim nisl? Aenean aliquet magna in nulla congue vehicula. Morbi dignissim tristique turpis sed sodales. In tincidunt dapibus semper. Nullam non orci eu massa tempus aliquam! Quisque placerat metus at neque aliquam sit amet iaculis lacus iaculis. Etiam ultrices condimentum justo eu viverra. Curabitur lacinia tristique </p>
+            <hr>
+            <h3 class="primary">Curabitur non orci sit amet magna scelerisque volutpat mauris ipsum metus</h3>
+            <p>Donec justo tellus, tincidunt vitae pellentesque nec, pharetra a orci. Praesent nunc nunc, egestas eget elementum sed; rutrum eget metus! Vestibulum congue congue dui ut porta. Aenean laoreet viverra turpis, a commodo purus eleifend a. Nam lacus dui; egestas ut aliquam nec, eleifend sit amet est. Nam justo elit, dictum id tempus a, ultricies tempus lacus. Nunc purus nibh; eleifend eget facilisis ac, sagittis non tortor. Vivamus eu enim a orci accumsan tincidunt ut ut elit. Vestibulum nisi orci, rutrum ac auctor non, viverra et magna? Sed vehicula sem et arcu tincidunt accumsan. In non pellentesque ipsum. Curabitur facilisis pellentesque pharetra. Donec justo urna, malesuada a viverra ac, pellentesque vitae nunc. Aenean ac leo eget nunc fringilla fringilla a non nulla! Nunc orci mi, venenatis quis ultrices vitae, congue non nibh. Nulla bibendum, justo eget ultrices vestibulum, erat tortor venenatis risus.<br/><br/>Quisque mauris risus, gravida a molestie eu, dictum ac augue. Integer sodales tempor lectus; sit amet dictum metus pharetra nec. Fusce bibendum dapibus pretium. Nunc eu sem vitae lacus laoreet elementum. Nulla facilisi. Phasellus ullamcorper elementum est, id pretium turpis bibendum vel. Aliquam a odio arcu. Morbi a dignissim nisl? Aenean aliquet magna in nulla congue vehicula. Morbi dignissim tristique turpis sed sodales. In tincidunt dapibus semper. Nullam non orci eu massa tempus aliquam! Quisque placerat metus at neque aliquam sit amet iaculis lacus iaculis. Etiam ultrices condimentum justo eu viverra. Curabitur lacinia tristique imperdiet. Aenean bibendum vehicula diam nec placerat. Donec lectus leo, consequat sit amet viverra et, lacinia eu lectus! Nullam convallis; justo a vestibulum interdum, ipsum mauris lobortis urna, in fringilla elit erat at est. Nullam erat risus, volutpat quis porttitor.</p>
+            <hr>
+            <h3 class="primary">Nunc pharetra orci nec urna euismod facilisis nullam eleifend enim et felis varius</h3>
+            <p>Nam lacus dui; egestas ut aliquam nec, eleifend sit amet est. Nam justo elit, dictum id tempus a, ultricies tempus lacus. Nunc purus nibh; eleifend eget facilisis ac, sagittis non tortor. Vivamus eu enim a orci accumsan tincidunt ut ut elit. Vestibulum nisi orci, rutrum ac auctor non, viverra et magna? Sed vehicula sem et arcu tincidunt accumsan. In non pellentesque ipsum. Curabitur facilisis pellentesque pharetra. Donec justo urna, malesuada a viverra ac, pellentesque vitae nunc. Aenean ac leo eget nunc fringilla fringilla a non nulla! Nunc orci mi, venenatis quis ultrices vitae, congue non nibh. Nulla bibendum, justo eget ultrices vestibulum, erat tortor venenatis risus. Quisque mauris risus, gravida a molestie eu, dictum ac augue. Integer sodales tempor lectus; sit amet dictum metus pharetra nec. Fusce bibendum dapibus pretium. Nunc eu sem vitae lacus laoreet elementum.<br/><br/>Phasellus ullamcorper elementum est, id pretium turpis bibendum vel. Aliquam a odio arcu. Morbi a dignissim nisl? Aenean aliquet magna in nulla congue vehicula. Morbi dignissim tristique turpis sed sodales. In tincidunt dapibus semper. Nullam non orci eu massa tempus aliquam! Quisque placerat metus at neque aliquam sit amet iaculis lacus iaculis. Etiam ultrices condimentum justo eu viverra. Curabitur lacinia tristique imperdiet. Aenean bibendum vehicula diam nec placerat. Donec lectus leo, consequat sit amet viverra et, lacinia eu lectus! Nullam convallis; justo a vestibulum interdum, ipsum mauris lobortis urna, in fringilla elit erat at est. Nullam erat risus, volutpat quis porttitor eget, egestas n odio. Aliquam erat volutpat. Proin eu purus sapien, eu accumsan dolor. Nunc iaculis ligula eget massa dictum quis porttitor orci elementum! In lobortis consectetur elit sed fringilla?</p><a href="mailto:#">privacy@demolink.org</a>
+          </div>
+        </section>
+      </main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
